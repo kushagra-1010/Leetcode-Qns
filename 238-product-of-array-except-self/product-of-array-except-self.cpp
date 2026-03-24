87a69a5222fdf12ddbc9a94e3ad02ad33645c59b
Kushagra Saxena <144206868+kushagra-1010@users.noreply.github.com>
2026-03-24 08:15:02 +0530

    Time: 3 ms (36.85%) | Memory: 42.4 MB (8.27%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> left(n);
+        vector<int> right(n);
+
+        left[0] = 1;
+        right[n-1] = 1;
+
+        // prefix
+        for(int i = 1; i < n; i++){
+            left[i] = nums[i-1] * left[i-1];
+        }
+
+        // suffix
+        for(int i = n-2; i >= 0; i--){
+            right[i] = nums[i+1] * right[i+1];
+        }
+
+        // answer
+        for(int i = 0; i < n; i++){
+            nums[i] = left[i] * right[i];
+        }
+
+        return nums;
+    }
+};

77fc34df328746ec9416ba726d46a3da0cd9c4da
Kushagra Saxena <144206868+kushagra-1010@users.noreply.github.com>
2026-04-06 02:50:48 +0530

    Time: 9 ms (76.63%) | Memory: 35 MB (87.30%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(vector<int>& weights, int n, int days, int maxLoad) {
+        int ships = 1;
+        int loads = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (weights[i] > maxLoad) return false;
+
+            if (loads + weights[i] <= maxLoad) {
+                loads += weights[i];
+            } else {
+                ships++;
+                loads = weights[i];
+
+                if (ships > days) return false;
+            }
+        }
+        return true;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+
+        int st = *max_element(weights.begin(), weights.end());
+        int end = accumulate(weights.begin(), weights.end(), 0);
+
+        int ans = -1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (isValid(weights, n, days, mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};

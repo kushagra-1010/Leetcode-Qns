77e4e7c8d392458793dc210bf986d9c31c8b57a7
Kushagra Saxena <144206868+kushagra-1010@users.noreply.github.com>
2026-05-05 20:27:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.86%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int solveMem(int m, int n, int i, int j,
-                 vector<vector<int>>& dp,
-                 vector<vector<int>>& grid) {
+    int solveMem(int i, int j, vector<vector<int>>& grid,
+                 vector<vector<int>>& dp) {
 
-        if(i < 0 || j < 0) return 0;
-        if(grid[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1;
+        if (i < 0 || j < 0) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (grid[i][j] == 1) return 0;
 
-        int up = solveMem(m, n, i - 1, j, dp, grid);
-        int left = solveMem(m, n, i, j - 1, dp, grid);
+        if (i == 0 && j == 0) return 1;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int up = solveMem(i - 1, j, grid, dp);
+        int left = solveMem(i, j - 1, grid, dp);
 
         return dp[i][j] = up + left;
     }
@@ -22,6 +23,6 @@ public:
 
         vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        return solveMem(m, n, m - 1, n - 1, dp, grid);
+        return solveMem(m - 1, n - 1, grid, dp);
     }
 };
